556ee19815320138ce1c91aa2f9bd3047ea45b27
coloredasterisk <coloredasterisk@gmail.com>
2023-02-05 14:42:46 -0700

    mirror

--- gloabal-jam-game/Assets/Scripts/MirrorProperties.cs
+++ gloabal-jam-game/Assets/Scripts/MirrorProperties.cs
@@ -6,10 +6,24 @@ public class MirrorProperties : MonoBehaviour
 {
 
     private GridComponent gc;
+    private SpriteRenderer sr;
+
+    public Sprite intialSprite;
+    public Sprite topleft;
+    public Sprite topRight;
+    public Sprite bottomleft;
+    public Sprite bottomright;
+
     // Start is called before the first frame update
     void Awake()
     {
         gc = GetComponent<GridComponent>();
+        sr = GetComponent<SpriteRenderer>();
+        intialSprite = sr.sprite;
+    }
+    public void ResetMirror()
+    {
+        sr.sprite = intialSprite;
     }
 
     public Vector2Int reflectLaser(TileType mirror, GridComponent cause) {
@@ -20,38 +34,46 @@ public class MirrorProperties : MonoBehaviour
         if (mirror == TileType.Mirror45 ) {
             if (GridManager.CheckItemAtPosition(gc, TileType.LaserHorizontal, leftPosition) != null){ //check if laser is on the left
                 //spawn laser upwards
+                sr.sprite = topleft;
                 return Vector2Int.up;
                 
 
             } else if (GridManager.CheckItemAtPosition(gc, TileType.LaserVertical, upPosition) != null) {
                 //spawn laser leftwards
+                sr.sprite = topleft;
                 return Vector2Int.left;
 
             }
             else if (GridManager.CheckItemAtPosition(gc, TileType.LaserHorizontal, rightPosition) != null) {
                 //spawn laser downwards
+                sr.sprite = bottomleft;
                 return Vector2Int.down;
             }
             else if (GridManager.CheckItemAtPosition(gc, TileType.LaserVertical, downPosition) != null) {
                 //spawn laser rightwards
+                sr.sprite = bottomleft;
                 return Vector2Int.right;
             }
         } else if (mirror == TileType.Mirror135) {
             if (GridManager.CheckItemAtPosition(gc, TileType.LaserHorizontal, leftPosition) != null){ //check if laser is on the left
                 //spawn laser downwards
+                sr.sprite = bottomright;
                 return Vector2Int.down;
 
             } else if (GridManager.CheckItemAtPosition(gc, TileType.LaserVertical, downPosition) != null) {
                 //spawn laser leftwards
+                sr.sprite = bottomright;
                 return Vector2Int.left;
 
             }
             else if (GridManager.CheckItemAtPosition(gc, TileType.LaserHorizontal, rightPosition) != null) {
                 //spawn laser upwards
+                sr.sprite = topRight;
                 return Vector2Int.up;
             }
             else if (GridManager.CheckItemAtPosition(gc, TileType.LaserVertical, upPosition) != null) {
                 //spawn laser rightwards
+                sr.sprite = topRight;
                 return Vector2Int.right;
             }
 
